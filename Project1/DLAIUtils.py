160786a4b1048a4483de7eb2d3b41afbaf413847
Shekhar Biswas <32758439+shekharbiswas@users.noreply.github.com>
2024-02-09 14:00:47 +0100

    Update DLAIUtils.py

--- Project1/DLAIUtils.py
+++ Project1/DLAIUtils.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from dotenv import load_dotenv, find_dotenv
-os.environ['OPENAI_API_KEY']  = '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'
 
 class Utils:
   def __init__(self):
@@ -11,7 +10,7 @@ class Utils:
     openai_key = ''
     if self.is_colab(): # google colab
       from google.colab import userdata
-      openai_key = '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'
+      openai_key = userdata.get("OPENAI_API_KEY")
     else: # jupyter notebook
       openai_key = os.getenv("OPENAI_API_KEY")
     return f'{index_name}-{openai_key[-36:].lower().replace("_", "-")}'
